60b3b76be6d981549cd205d0bdcd953a662a3cd3
Ian Ale <ianalebom@gmail.com>
2026-07-11 23:38:14 +0700

    fix halaman survey

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -1,17 +1,26 @@
 import { AppContent } from '@/components/app-content';
 import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
+import { usePage } from '@inertiajs/react';
 import { GlobalSurveySelector } from '@/components/global-survey-selector';
 import type { AppLayoutProps } from '@/types';
 
 export default function AppSidebarLayout({ children }: AppLayoutProps) {
+    const { component } = usePage();
+    const hideSurveySelector = [
+        'survey/take-survey',
+        'survey-assignment/take-survey-pariwisata',
+    ].includes(component);
+
     return (
         <AppShell variant="sidebar">
             <AppSidebar />
             <AppContent variant="sidebar">
-                <div className="sticky top-0 z-[1050] flex flex-col">
-                    <GlobalSurveySelector />
-                </div>
+                {!hideSurveySelector && (
+                    <div className="sticky top-0 z-[1050] flex flex-col">
+                        <GlobalSurveySelector />
+                    </div>
+                )}
                 {children}
             </AppContent>
         </AppShell>
